317d0907405f84b3ab77cc85aa500d476f02e22d
typon <hfarooq@protonmail.com>
2022-09-02 00:07:11 -0400

    update setup tools script and fix bug in zshrc

--- setup_tools.py
+++ setup_tools.py
@@ -8,10 +8,19 @@ class Context:
 
 def wait_for_enter():
     input("Press Enter to continue...")
+    print()
 
 
 def incr_step(ctx):
     ctx.step += 1
+    return ctx
+
+
+def install_terminal_font(ctx):
+    print(f"Step {ctx.step}: Install Jetbrains Mono Font")
+    print("https://www.jetbrains.com/lp/mono/")
+    wait_for_enter()
+    ctx = incr_step(ctx)
     return ctx
 
 
@@ -22,6 +31,15 @@ def install_kitty(ctx):
     ctx = incr_step(ctx)
     return ctx
 
+def install_tmux_conf(ctx):
+    print(f"Step {ctx.step}: Install tmux config")
+    print("cd $HOME")
+    print("git clone https://github.com/gpakosz/.tmux.git")
+    print("ln -s -f .tmux/.tmux.conf")
+    print("cp .tmux/.tmux.conf.local .")
+    wait_for_enter()
+    ctx = incr_step(ctx)
+    return ctx
 
 def install_brew(ctx):
     print(f"Step {ctx.step}: Install Linuxbrew")
@@ -30,6 +48,28 @@ def install_brew(ctx):
     ctx = incr_step(ctx)
     return ctx
 
+def install_zsh(ctx):
+    print(f"Step {ctx.step}: Install zsh")
+    print('sudo apt-get install zsh')
+    wait_for_enter()
+    ctx = incr_step(ctx)
+    return ctx
+
+def copy_zshrc(ctx):
+    print(f"Step {ctx.step}: Copy zshrc from dotfiles repo")
+    print("cp .zshrc $HOME/")
+    wait_for_enter()
+    ctx = incr_step(ctx)
+    return ctx
+
+def copy_other_dotfiles(ctx):
+    print(f"Step {ctx.step}: Copy other dotfiles")
+    print("cp .exports $HOME/")
+    print("cp .funcs $HOME/")
+    print("cp .aliases $HOME/")
+    wait_for_enter()
+    ctx = incr_step(ctx)
+    return ctx
 
 def install_oh_my_zsh(ctx):
     print(f"Step {ctx.step}: Install oh-my-zsh")
@@ -45,15 +85,6 @@ def install_powerlevel10k(ctx):
     wait_for_enter()
     ctx = incr_step(ctx)
     return ctx
-
-
-def install_powerlevel10k(ctx):
-    print(f"Step {ctx.step}: Install powerlevel10k")
-    print('git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k')
-    wait_for_enter()
-    ctx = incr_step(ctx)
-    return ctx
-
 
 def install_zsh_syntax_highlighter(ctx):
     print(f"Step {ctx.step}: Install zsh syntax highligher")
@@ -79,18 +110,39 @@ def install_neovim(ctx):
     ctx = incr_step(ctx)
     return ctx
 
+def install_lazygit(ctx):
+    print(f"Step {ctx.step}: Install lazygit")
+    print("brew install lazygit")
+    wait_for_enter()
+    ctx = incr_step(ctx)
+    return ctx
+
+def install_ripgrep(ctx):
+    print(f"Step {ctx.step}: Install ripgrep")
+    print("brew install ripgrep")
+    wait_for_enter()
+    ctx = incr_step(ctx)
+    return ctx
+
 
 if __name__ == "__main__":
     ctx = Context(step=0)
 
     steps = [
+        install_terminal_font,
         install_kitty,
+        install_tmux_conf,
         install_brew,
+        install_zsh,
         install_oh_my_zsh,
+        copy_zshrc,
+        copy_other_dotfiles,
         install_powerlevel10k,
         install_zsh_syntax_highlighter,
         install_fzf,
         install_neovim,
+        install_lazygit,
+        install_ripgrep,
     ]
     for step in steps:
          ctx = step(ctx)
