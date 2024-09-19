5eead81f0457b7ff62bd5f4def75008001e8efd7
typon <hassan.typon@gmail.com>
2024-09-18 17:05:19 -0700

    check zsh, install tmux up

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 set -e
 set -x
 
+# Check if zsh is installed
+if ! command -v zsh &> /dev/null
+then
+    echo "zsh is not installed. Please install zsh before proceeding."
+    exit 1
+fi
+
 # Initialize arguments
 install_homebrew=false
 
@@ -25,12 +32,17 @@ cd $HOME
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-
 # Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install tmux config
+cd $HOME
+rm -rf ~/.tmux
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ln -s -f .tmux/.tmux.conf
 
 # Download dotfiles from github
 cd ~/.dotfiles
@@ -43,14 +55,8 @@ cp .tmux.conf.local ~/
 source ~/.exports
 # install brew tools
 if [ "$install_homebrew" = true ]; then
-    brew install bat ripgrep lazygit btop fzf neovim lazydocker git-delta knqyf263/pet/pet
+    brew install eza bat ripgrep lazygit btop fzf neovim lazydocker git-delta knqyf263/pet/pet
 fi
 
 # Install kickstart nvim
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-
-# Install tmux config
-cd $HOME
-rm -rf ~/.tmux
-git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-ln -s -f .tmux/.tmux.conf
